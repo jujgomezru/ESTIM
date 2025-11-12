6567e252aa3fe9c907cbec1a77e535a2f48223a6
jujgomezru <jujgomezru@unal.edu.co>
2025-11-12 09:45:25 -0500

    Final folder management of architecture starting point

---
+++ Final-Project/apps/java-api/src/main/java/com/estim/javaapi/presentation/HealthController.java
@@ -0,0 +1,12 @@
+package com.estim.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "java-api:ok";
+    }
+}
