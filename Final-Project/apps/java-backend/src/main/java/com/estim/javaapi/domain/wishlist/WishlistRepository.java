6f5ce490c3bfa86b8d4eaff0b476a3fe3168d7e0
jujgomezru <jujgomezru@unal.edu.co>
2025-11-17 18:54:27 -0500

    Architecture of missing functionalities of java backend

---
+++ Final-Project/apps/java-backend/src/main/java/com/estim/javaapi/domain/wishlist/WishlistRepository.java
@@ -0,0 +1,4 @@
+package com.estim.javaapi.domain.wishlist;
+
+public class WishlistRepository {
+}
---
+++ Final-Project/apps/java-backend/src/main/java/com/estim/javaapi/domain/wishlist/events/WishlistNotificationTriggered.java
@@ -0,0 +1,4 @@
+package com.estim.javaapi.domain.wishlist.events;
+
+public class WishlistNotificationTriggered {
+}
