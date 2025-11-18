fe535c9c01599c9c72cee4d660bb7440aca15ead
john paez <jopaeza@unal.edu.co>
2025-11-17 23:27:46 -0500

    adelantos del frontend en Final-Project/web/

---
+++ Final-Project/web/src/api/endpoints.js
@@ -0,0 +1,7 @@
+// me dice que endpoints puede utiliar
+export const ENDPOINTS = {
+  REGISTER: "/auth/register", //enpoint de la api
+  LOGIN: "/auth/login",
+  CARD: "/card",
+  SIMPSON_CHARACTER: "https://thesimpsonsapi.com/api/characters/1",//simpsons
+};
